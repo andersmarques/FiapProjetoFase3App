704c93b2ab952631c934011a03cbcccace504738
Anderson Marques <marques_sep@hotmail.com>
2022-10-09 15:53:34 -0300

    Flutter

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const url = 'http://abctechapi-env-1.eba-rrkmnh2y.us-east-1.elasticbeanstalk.com';
+}
---
+++ test/resources/assist_response.js
@@ -0,0 +1,17 @@
+[
+	{
+		"id": 1,
+		"name": "Troca de aparelho",
+		"description": "Troca de aparelho decodificador de sinal"
+	},
+	{
+		"id": 2,
+		"name": "Troca de cabo interno",
+		"description": "Troca de cabo interno"
+	},
+	{
+		"id": 3,
+		"name": "Troca de fiação interna",
+		"description": "Troca de fiação interna"
+	}
+]
