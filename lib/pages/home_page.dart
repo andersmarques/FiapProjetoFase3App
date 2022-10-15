47365aaa168b5f5bf07525f10e3dd576f6c181d5
Anderson Marques <marques_sep@hotmail.com>
2022-10-15 16:55:14 -0300

    Ultima aula

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,50 +4,56 @@ import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
 class HomePage extends GetView<AssistController> {
-  
   const HomePage({super.key});
 
-  Widget renderAssist(List<Assist> assist){
+  Widget renderAssist(List<Assist> assist) {
     return ListView.builder(
-      shrinkWrap: true,
-      itemCount: assist.length,
-      itemBuilder: ((context, index) =>
-         ListTile(title: Text(assist[index].name))));
+        shrinkWrap: true,
+        itemCount: assist.length,
+        itemBuilder: ((context, index) => ListTile(
+              title: Text(assist[index].name),
+              selectedColor: Colors.red,
+              selected: controller.isSelected(index),
+              onTap: () => controller.selectAssist(index),
+            )));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text('Lista de serviços'),
-        ),
-        body: Container(
+        title: const Text('Lista de serviços'),
+      ),
+      body: Container(
           constraints: const BoxConstraints.expand(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                children: const [
-                  Expanded(
-                  child: Text('Os Serviçoes disponíveis são', 
-                  	textAlign: TextAlign.center,))
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                  		child: TextButton(
-                    onPressed: controller.getAssistList,
-                    child: const Text('Recarregar'),
-                  ))
-                ],
-              ),
-              controller.obx(
-              (state) => renderAssist(state ?? []),
-                onError:(error) => Text(error.toString()),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: const [
+                    Expanded(
+                        child: Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Text(
+                              'Os serviços disponíveis são:',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            )))
+                  ],
+                ),
+                controller.obx(
+                  (state) => renderAssist(state ?? []),
+                  onError: (error) => Text(error.toString()),
+                  onEmpty: const Text("Nenhuma assistência disponível"),
                 )
-            ],
-          )
-        ),
+              ],
+            ),
+          )),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => controller.finishSelectAssist(),
+          child: const Icon(Icons.done)),
     );
   }
 }
